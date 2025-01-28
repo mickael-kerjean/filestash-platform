05d5606881bca7e5497142348ca9d8ac65e4cdbc
MickaelK <mickael@kerjean.me>
2025-01-28 12:38:35 +1100

    fix (test): docker registry page test

---
+++ src/test/java/app/filestash/platform/registry/DockerRegistryControllerTest.java
@@ -0,0 +1,33 @@
+package app.filestash.platform.registry;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class DockerRegistryControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+	@Test
+    void testDeploymentDocker() throws Exception {
+        // given
+        Document document = Jsoup.parse(mockMvc.perform(get("/enterprise:latest"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString());
+
+        // then
+        assertThat(document.select("h2").text()).contains("Docker Registry");
+    }
+}
